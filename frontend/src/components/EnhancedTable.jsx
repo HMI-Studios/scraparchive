6bab8a120b59bd38f98e9cc8bf9edb73f5ccc020
Johannes Niemelä <johannes@niemela.se>
2024-06-06 21:33:03 -0500

    add enhanced table component

---
+++ frontend/src/components/EnhancedTable.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import TextBtn from './buttons/TextBtn.jsx';
+
+function descendingComparator(a, b, column, defaultColumn) {
+  let aVal = a[column.id];
+  let bVal = b[column.id];
+
+  if (column.numeric) {
+    aVal = Number(a[column.id]);
+    bVal = Number(b[column.id]);
+  }
+
+  if (!aVal) return -1;
+  if (!bVal) return 1;
+
+  if (aVal < bVal) {
+    return -1;
+  }
+  if (aVal > bVal) {
+    return 1;
+  }
+  if (defaultColumn) return descendingComparator(a, b, defaultColumn);
+  else return 0;
+}
+
+function getComparator(orderDesc, column, defaultColumn) {
+  return orderDesc
+    ? (a, b) => descendingComparator(a, b, column, defaultColumn)
+    : (a, b) => -descendingComparator(a, b, column, defaultColumn);
+}
+
+class EnhancedTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sortBy: props.defaultSort,
+      orderDesc: true,
+    };
+
+    this.sortBy = this.sortBy.bind(this);
+  }
+
+  sortBy(id) {
+    let { orderDesc, sortBy } = this.state;
+    if (sortBy === id) {
+      orderDesc = !orderDesc;
+    } else {
+      sortBy = id;
+    }
+    this.setState({ orderDesc, sortBy });
+  }
+
+  getStrValue(column, row) {
+    let value = row[column.id];
+
+    if (column.isDate) {
+      return value ? value.toDateString() : 'Never';
+    } else {
+      return value;
+    }
+  }
+
+  render() {
+    const { columns, rows, refresh, onClicks, links, defaultSort } = this.props;
+    const { orderDesc, sortBy } = this.state;
+
+    const sortColumn = columns.find(column => column.id === sortBy);
+    const defaultColumn = columns.find(column => column.id === defaultSort);
+
+    const sortedRows = sortBy ? rows.sort(getComparator(orderDesc, sortColumn, defaultColumn)) : rows;
+
+    return (
+      <div className="enhancedTable">
+        <div className="tableBtns">
+          <TextBtn onClick={refresh}>Refresh</TextBtn>
+        </div>
+        <div className="tableFrame">
+          <table>
+            <thead>
+              <tr>
+                {columns.map(column => (
+                  <th key={`HEAD-${column.id}`} onClick={() => this.sortBy(column.id)}>{column.label}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {sortedRows.map(row => (
+                <tr key={row.id}>
+                  {columns.map((column, i) => {
+                    let content = <>{column.prefix}{this.getStrValue(column, row)}</>;
+                    if (onClicks && onClicks[column.id]) {
+                      content = <a className="tableLink" onClick={() => onClicks[column.id](row)}>{content}</a>;
+                    } else if (links && links[column.id]) {
+                      content = <Link className="tableLink" to={`${window.ADDR_PREFIX}${links[column.id](row)}`}>{content}</Link>;
+                    }
+
+                    return <td key={`${row.id}-${column.id}`} className={i > 0 ? 'leftBorder' : ''}>{content}</td>;
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EnhancedTable;
+  
