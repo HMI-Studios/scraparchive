65c2007616d6247c4d6186e8f4bb745dd7394d07
Johannes Niemelä <johannes@niemela.se>
2024-06-14 15:57:24 -0500

    fix axios export

--- frontend/src/configuredAxios.js
+++ frontend/src/configuredAxios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-export default axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.example.com', // Replace with your API base URL
   withCredentials: true,
 });
+
+export default axiosInstance;
