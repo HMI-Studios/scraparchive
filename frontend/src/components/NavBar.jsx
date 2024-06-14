c9bf29d4839cdcb363714701bc98d3b915fb42a9
Johannes Niemelä <johannes@niemela.se>
2024-06-14 14:26:17 -0500

    rebrand back to ScrapArchive

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = ({ setView, user }) => (
         className="logoText"
         to={`${window.ADDR_PREFIX}`}
       >
-        Scrapbook
+        ScrapArchive
       </Link>
     </div>
     <div className="navMenu center">
